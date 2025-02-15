73485cd5a5459c7849cb0213b4f410c68233eb67
Sayfullah Sayeb <sayfullah.sayeb@gmail.com>
2025-02-15 12:06:02 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+(function() {
+    const original = Runner.prototype.gameOver;
+    Runner.prototype.gameOver = function() {};
+
+    function autoJump() {
+        const obstacles = Runner.instance_.horizon.obstacles;
+        if (obstacles.length > 0) {
+            const obstacle = obstacles[0];
+            const distance = obstacle.xPos - Runner.instance_.tRex.xPos;
+            const height = obstacle.yPos;
+            
+            if (distance < 100 && height > 75 && Runner.instance_.tRex.jumping === false) {
+                Runner.instance_.tRex.startJump(0.5);
+            }
+        }
+        requestAnimationFrame(autoJump);
+    }
+    
+    requestAnimationFrame(autoJump);
+})();
