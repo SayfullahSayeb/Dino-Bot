5da2f402d38da6edc9ea965bf93996237c1424f3
Sayfullah Sayeb <sayfullah.sayeb@gmail.com>
2025-03-06 04:29:11 +0600

    update logic and UI

--- script.js
+++ script.js
@@ -102,39 +102,49 @@
     });
 
     // Toggle the "Auto Play" feature
+    document.getElementById("AutoPlayToggle").addEventListener("change", function () {
+        if (this.checked) {
+            // If Auto Play is turned on, start auto-play interval
+            autoPlayInterval = setInterval(function () {
+                const KEY_CODE_SPACE_BAR = 32;
+                const KEY_CODE_ARROW_DOWN = 40;
+                const CANVAS_HEIGHT = Runner.instance_.dimensions.HEIGHT;
+                const DINO_HEIGHT = Runner.instance_.tRex.config.HEIGHT;
+
+                const obstacle = Runner.instance_.horizon.obstacles[0];
+                const speed = Runner.instance_.currentSpeed;
+
+                if (obstacle) {
+                    const w = obstacle.width;
+                    const x = obstacle.xPos; // measured from left of canvas
+                    const y = obstacle.yPos; // measured from top of canvas
+                    const yFromBottom = CANVAS_HEIGHT - y - obstacle.typeConfig.height;
+                    const isObstacleNearby = x < 25 * speed - w / 2;
+
+                    if (isObstacleNearby) {
+                        if (yFromBottom > DINO_HEIGHT) {
+                            // Pterodactyl going from above, do nothing
+                        } else if (y > CANVAS_HEIGHT / 2) {
+                            // Jump
+                            dispatchKey("keyup", KEY_CODE_ARROW_DOWN);
+                            dispatchKey("keydown", KEY_CODE_SPACE_BAR);
+                        } else {
+                            // Duck
+                            dispatchKey("keydown", KEY_CODE_ARROW_DOWN);
+                        }
+                    }
+                }
+            }, Runner.instance_.msPerFrame); // Run the auto-play every frame
+        } else {
+            // Stop auto-play if the toggle is unchecked
+            clearInterval(autoPlayInterval);
+        }
+    });
+
+    // Function to simulate keypress events
     function dispatchKey(type, key) {
         document.dispatchEvent(new KeyboardEvent(type, { keyCode: key }));
     }
-    autoPlayInterval = setInterval(function () {
-        const KEY_CODE_SPACE_BAR = 32;
-        const KEY_CODE_ARROW_DOWN = 40;
-        const CANVAS_HEIGHT = Runner.instance_.dimensions.HEIGHT;
-        const DINO_HEIGHT = Runner.instance_.tRex.config.HEIGHT;
-
-        const obstacle = Runner.instance_.horizon.obstacles[0];
-        const speed = Runner.instance_.currentSpeed;
-
-        if (obstacle) {
-            const w = obstacle.width;
-            const x = obstacle.xPos; // measured from left of canvas
-            const y = obstacle.yPos; // measured from top of canvas
-            const yFromBottom = CANVAS_HEIGHT - y - obstacle.typeConfig.height;
-            const isObstacleNearby = x < 25 * speed - w / 2;
-
-            if (isObstacleNearby) {
-                if (yFromBottom > DINO_HEIGHT) {
-                    // Pterodactyl going from above, do nothing
-                } else if (y > CANVAS_HEIGHT / 2) {
-                    // Jump
-                    dispatchKey("keyup", KEY_CODE_ARROW_DOWN);
-                    dispatchKey("keydown", KEY_CODE_SPACE_BAR);
-                } else {
-                    // Duck
-                    dispatchKey("keydown", KEY_CODE_ARROW_DOWN);
-                }
-            }
-        }
-    }, Runner.instance_.msPerFrame); // Run the auto-play every frame
 
     // Event listener to close the bot UI
     document.getElementById("closeBotUI").addEventListener("click", () => botUI.remove());
